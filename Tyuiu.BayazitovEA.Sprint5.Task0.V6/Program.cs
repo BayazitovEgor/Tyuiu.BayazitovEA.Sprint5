8af0951edd745bd7da434a55bfd02fe5f1d8c9fb
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-11-23 16:23:28 +0500

    Add project files.

---
+++ Tyuiu.BayazitovEA.Sprint5.Task0.V6/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.BayazitovEA.Sprint5.Task0.V6.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = 3;
+        DataService ds = new DataService();
+
+        string res =  ds.SaveToFileTextData(x);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
