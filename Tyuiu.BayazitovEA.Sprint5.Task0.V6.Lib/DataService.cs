7e31adfdf99f3c2adaa33e36cb72d57620747a79
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-11-23 16:36:15 +0500

    0

--- Tyuiu.BayazitovEA.Sprint5.Task0.V6.Lib/DataService.cs
+++ Tyuiu.BayazitovEA.Sprint5.Task0.V6.Lib/DataService.cs
@@ -5,11 +5,13 @@ namespace Tyuiu.BayazitovEA.Sprint5.Task0.V6.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
-            double z = x/(Math.Sqrt(Math.Pow(x,2)+x));
-            z=Math.Round(z,3);
-            File.WriteAllText(path,Convert.ToString(z));
-            return path;
+            string path = "OutputFileTask0.txt"; // Просто имя файла
+
+            double z = x / (Math.Sqrt(Math.Pow(x, 2) + x));
+            z = Math.Round(z, 3);
+
+            File.WriteAllText(path, z.ToString());
+            return Path.GetFullPath(path);
         }
     }
 }
