cd77f2c889e85e7aa3954f6894b885041a9eb3ba
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-11-24 20:31:34 +0500

    3

---
+++ Tyuiu.BayazitovEA.Sprint5.Task3.V15.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BayazitovEA.Sprint5.Task3.V15.Lib
+{
+    public class DataService : ISprint5Task3V15
+    {
+        public string SaveToFileTextData(int x)
+        {
+            // F(x) = -1.4x³ + 2.3x² + 0.6x
+            double result = -1.4 * Math.Pow(x, 3) + 2.3 * Math.Pow(x, 2) + 0.6 * x;
+            result = Math.Round(result, 3);
+
+            // Сохраняем в бинарный файл
+            string tempPath = Path.GetTempPath();
+            string outputFile = Path.Combine(tempPath, "OutPutFileTask3.bin");
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(outputFile, FileMode.Create)))
+            {
+                writer.Write(result);
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint5.Task3.V15/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BayazitovEA.Sprint5.Task3.V15.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = 5;
+
+        DataService ds = new DataService();
+        string res = ds.SaveToFileTextData(x);
+
+        Console.WriteLine(res);
+
+        Console.ReadKey();
+    }
+}
