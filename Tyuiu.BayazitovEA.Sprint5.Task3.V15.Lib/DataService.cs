c5f2c35da0d23380a8c3bc6d4d58f26ff90d7048
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-11-24 21:40:22 +0500

    3

--- Tyuiu.BayazitovEA.Sprint5.Task3.V15.Lib/DataService.cs
+++ Tyuiu.BayazitovEA.Sprint5.Task3.V15.Lib/DataService.cs
@@ -5,27 +5,21 @@ namespace Tyuiu.BayazitovEA.Sprint5.Task3.V15.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            // F(x) = -1.4x³ + 2.3x² + 0.6x
+     
             double result = -1.4 * Math.Pow(x, 3) + 2.3 * Math.Pow(x, 2) + 0.6 * x;
             result = Math.Round(result, 3);
 
-            // Создаем бинарные данные
-            byte[] binaryData;
-            using (MemoryStream stream = new MemoryStream())
-            using (BinaryWriter writer = new BinaryWriter(stream))
+            string tempPath = Path.GetTempPath();
+            string path = Path.Combine(tempPath, "OutPutFileTask3.bin");
+
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
             {
-                writer.Write(result);
-                binaryData = stream.ToArray();
+                writer.Write(BitConverter.GetBytes(result));
             }
 
-            // Сохраняем в файл
-            string tempPath = Path.GetTempPath();
-            string outputFile = Path.Combine(tempPath, "OutPutFileTask3.bin");
-            File.WriteAllBytes(outputFile, binaryData);
 
-            // Возвращаем base64 строку
-            string base64String = Convert.ToBase64String(binaryData);
-            return base64String;
+            return path;
         }
     }
 }
