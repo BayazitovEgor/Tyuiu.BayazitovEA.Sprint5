6470406f92d27c438c9b3ccf22f956feb5ad3e40
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-11-25 20:44:50 +0500

    5

---
+++ Tyuiu.BayazitovEA.Sprint5.Task5.V30.Lib/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BayazitovEA.Sprint5.Task5.V30.Lib
+{
+    public class Class1 : ISprint5Task5V30
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double x = 0;
+            string data = File.ReadAllText(path);
+            string[] strings = data.Split(' ');
+
+            foreach (string s in strings) 
+            {
+                if (int.TryParse(s.Trim(), out int number) && number % 1 == 0 && number>x)
+                { x = number; }
+            }
+            return x;
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint5.Task5.V30/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BayazitovEA.Sprint5.Task5.V30.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string x = $@"C:\\DataSprint5\InPutDataFileTask5V30.txt";
+
+        Class1 ds = new Class1();
+        double res = ds.LoadFromDataFile(x);
+
+        Console.WriteLine(res);
+
+        Console.ReadKey();
+    }
+}
